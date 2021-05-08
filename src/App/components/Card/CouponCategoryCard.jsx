5ff1e7cf365f6a7d3b18191b8689543a159e0018
René Müller <rene.mueller@clicksports.de>
2021-05-08 15:35:23 +0200

    adjustments for CouponCategoryCard.jsx: added color_background and color_foreground to items

--- src/App/components/Card/CouponCategoryCard.jsx
+++ src/App/components/Card/CouponCategoryCard.jsx
@@ -11,10 +11,10 @@ const CouponCategoryCard = ({item, history, ...htmlProps}) => {
         <IonCol size={6} sizeMd={4} sizeLg={3} sizeXl={2}>
             <IonCard routerLink={link(item.id)}
                  className="coupon-category-card"
-                 style={{backgroundColor: item.color}}
+                 style={{backgroundColor: item.color_background}}
                  {...htmlProps}>
 
-                <IonCardTitle className="coupon-category-card-name">
+                <IonCardTitle className="coupon-category-card-name" style={{color: item.color_foreground}}>
                     {item.name}
                 </IonCardTitle>
             </IonCard>

fa4b58f7daea18277db5e9dcd9107f4e38387db8
René Müller <mail@rene-mueller.xyz>
2022-02-19 14:26:51 +0100

    switched CouponList to typescript

--- src/App/components/Coupon/CouponList/CouponList.tsx
+++ src/App/components/Coupon/CouponList/CouponList.tsx
@@ -1,9 +1,19 @@
-import React, {useMemo} from "react";
+import React, {FC, useMemo} from "react";
 import {IonList, IonItemGroup, IonListHeader, IonLabel} from "@ionic/react";
 import CouponItem from "../CouponItem/CouponItem";
 import {useAppSelector} from "../../../../store";
+import {CouponCategoryProperties} from "../../../../api/services/coupon-categories";
+import {CouponProperties} from "../../../../api/services/coupons";
 
-const CouponList = ({partner, ionRouterOutlet}) => {
+interface CouponListProps {
+    partner: CouponCategoryProperties,
+}
+
+interface CouponGroupList {
+    [x: string]: CouponProperties[]
+}
+
+const CouponList: FC<CouponListProps> = ({partner}) => {
     const coupons = useAppSelector(state => state.coupons.coupons.filter(item => item.coupon_category_id === partner.id));
 
     /* group coupons */
@@ -12,7 +22,7 @@ const CouponList = ({partner, ionRouterOutlet}) => {
             return;
         }
 
-        return coupons.reduce((rv, x) => {
+        return coupons.reduce((rv: CouponGroupList, x) => {
             rv[x.condition] = [...rv[x.condition] || [], x];
             return rv;
         }, {});
@@ -30,7 +40,7 @@ const CouponList = ({partner, ionRouterOutlet}) => {
                         <IonLabel>{key}</IonLabel>
                     </IonListHeader>
                     {value.map((item, index) => (
-                        <CouponItem key={index} item={item} partner={partner} ionRouterOutlet={ionRouterOutlet}/>
+                        <CouponItem key={index} item={item}/>
                     ))}
                 </IonItemGroup>
             ))}

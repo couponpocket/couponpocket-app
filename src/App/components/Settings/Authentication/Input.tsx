66f2a82e37bf880af5c0b172a3410ecdf3612269
René Müller <48084558+rene-mueller@users.noreply.github.com>
2022-08-28 12:05:14 +0200

    added error clear function for specific input on input change

--- src/App/components/Settings/Authentication/Input.tsx
+++ src/App/components/Settings/Authentication/Input.tsx
@@ -1,8 +1,9 @@
-import React, {Dispatch, FC, SetStateAction} from 'react';
+import React, {Dispatch, FC, SetStateAction, useCallback} from 'react';
 import {IonInput, IonLabel, IonText} from '@ionic/react';
-import {TextFieldTypes} from '@ionic/core';
+import {InputChangeEventDetail, TextFieldTypes} from '@ionic/core';
 
 import './Input.css';
+import {IonInputCustomEvent} from '@ionic/core/dist/types/components';
 
 export interface ErrorState {
     [val: string]: string[];
@@ -11,13 +12,31 @@ export interface ErrorState {
 interface InputProps {
     label?: string;
     placeholder: string;
-    name?: string;
+    name: string;
     type?: TextFieldTypes;
-    errors: ErrorState | undefined,
-    setErrors: Dispatch<SetStateAction<ErrorState | undefined>>
+    errors: ErrorState | undefined;
+    setErrors: Dispatch<SetStateAction<ErrorState | undefined>>;
 }
 
-const Input: FC<InputProps> = ({label, placeholder, name, type}) => {
+type inputChange = (event: IonInputCustomEvent<InputChangeEventDetail>) => void;
+
+const Input: FC<InputProps> = ({label, placeholder, name, type, errors, setErrors}) => {
+    const inputChange: inputChange = useCallback((e) => {
+        e.preventDefault();
+
+        setErrors((errors) => {
+            if (!errors) {
+                return errors;
+            }
+
+            // is required, because the state won't update if we don't copy the state
+            const copy = {...errors};
+
+            delete copy[e.target.name];
+            return copy;
+        });
+    }, [setErrors]);
+
     return (
         <>
             {label === undefined ? (
@@ -26,7 +45,7 @@ const Input: FC<InputProps> = ({label, placeholder, name, type}) => {
                 </IonLabel>
             ) : null}
             <div className="input-field">
-                <IonInput type={type} name={name} placeholder={placeholder}/>
+                <IonInput onIonChange={inputChange} type={type} name={name} placeholder={placeholder}/>
             </div>
             {errors && Object.keys(errors).includes(name) ? (
                 <IonText color="danger">{errors[name].toString()}</IonText>

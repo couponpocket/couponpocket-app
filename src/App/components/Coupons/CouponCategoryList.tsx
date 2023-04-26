d149ea57c521363d2fbaef127f8ab2fe5e4b397c
René Müller <48084558+rene-mueller@users.noreply.github.com>
2023-04-26 08:26:22 +0200

    added hint, when no coupons are available

--- src/App/components/Coupons/CouponCategoryList.tsx
+++ src/App/components/Coupons/CouponCategoryList.tsx
@@ -1,8 +1,26 @@
 import React, {useMemo} from 'react';
 
-import {IonGrid, IonRow} from '@ionic/react';
+import {
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonGrid,
+    IonIcon,
+    IonRow,
+    IonText
+} from '@ionic/react';
 import CouponCategoryItem from './CouponCategoryItem';
 import {useAppSelector} from '../../../store';
+import {
+    addCircleOutline,
+    addCircleSharp,
+    barcodeOutline,
+    barcodeSharp,
+} from "ionicons/icons";
+
+import './couponCategoryList.css';
 
 const CouponCategoryList = () => {
     const {categories, coupons} = useAppSelector(state => state.coupons);
@@ -12,7 +30,7 @@ const CouponCategoryList = () => {
         return categories.filter(i => uniqueCategoriesByCoupons.includes(i.id));
     }, [categories, coupons]);
 
-    return (
+    return filteredCategories.length ? (
         <IonGrid>
             <IonRow className="coupon-category-list">
                 {filteredCategories.map((item, index) => (
@@ -20,6 +38,23 @@ const CouponCategoryList = () => {
                 ))}
             </IonRow>
         </IonGrid>
+    ) : (
+        <IonCard>
+            <div className="coupon-category-list-empty">
+                <IonIcon className="coupon-category-list-empty-icon" ios={barcodeOutline} md={barcodeSharp}/>
+                <IonIcon className="coupon-category-list-empty-icon" ios={addCircleOutline} md={addCircleSharp}/>
+            </div>
+            <IonCardHeader>
+                <IonCardTitle>Keine Coupons verfügbar</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+                <IonText>
+                    Füge jetzt die Coupons aus deiner Punktepost hinzu um um diese mit der Community zu teilen und so
+                    gemeinsam noch mehr von Payback zu profitieren.
+                </IonText>
+            </IonCardContent>
+            <IonButton fill="clear" disabled>Jetzt hinzufügen</IonButton>
+        </IonCard>
     );
 };
 
